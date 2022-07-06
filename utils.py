96c4e7606e2e9ef0b87f0f0803bb8c4c0cbdf930
an-Underpriviliged-ZJUer <3190103335@zju.edu.cn>
2022-07-06 15:45:33 +0800

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# @Description :
+# @File : utils.py
+# @Time : 2022/7/6 15:39
+# @Author : HarrisonWu42
+# @Email: harrisonwu.com@gmail.com
+# @Software: PyCharm
+
+
+import pandas as pd
+import numpy as np
+import random
+
+
+def get_data(dpAddress, sql):
+	url = "http://www.citybrain.org/api/getData"
+	data = {"dpAddress": dpAddress, "payload": "{\"selectSql\":\"" + sql + "\"}"}
+	resp = requests.post(url=url, headers={"content-type": "application/json"}, json=data)
+	result = resp.json()
+	data=result["data"]
+	
+	datalist = data[2:-3].split('=[')
+	header_name = datalist[1].split("]")
+	header_name_2 = header_name[0].split(',')
+	print(header_name_2)
+	P = datalist[-1].split(",")
+	value_list = []
+	for i in range(len(P)):
+	    value_list.append(P[i].split(';'))
+	
+	df4 = pd.DataFrame(data=value_list, columns=header_name_2)
+	return df4
